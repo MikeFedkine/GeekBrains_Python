82f70f2a4b68ba34754c3fafa91be7b6df5e16df
Mike Fedkin <m.fedkine@gmail.com>
2022-04-19 18:53:11 +0300

    Update Task2.py
    Исправлена ошибка в часах (ранее деление было на 360..., а должно быть на 3600

--- Task2.py
+++ Task2.py
@@ -6,9 +6,9 @@
 time = input("Введите время в секундах: ")
 time_new = int(time)
 
-hours = time_new // 360
-minutes = (time_new-hours*360) // 60
-seconds = time_new - hours*360 - minutes*60
+hours = time_new // 3600
+minutes = (time_new-hours*3600) // 60
+seconds = time_new - hours*3600 - minutes*60
 
 
 print("Переведенное время: ", hours, ":", minutes, ":", seconds)
