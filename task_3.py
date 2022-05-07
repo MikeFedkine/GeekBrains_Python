a308ee2b5b7866dca4e8f04a5fa8b550618349bd
Mike Fedkin <m.fedkine@gmail.com>
2022-05-07 18:00:37 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,23 @@
+"""
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+"""
+
+def my_func(arg_1 , arg_2, arg_3):
+    if arg_1 >= arg_2 and arg_1 >= arg_3:
+        if arg_2 >= arg_3:
+            return arg_1 + arg_2
+        else:
+            return arg_1 + arg_3
+    elif arg_2 >= arg_1 and arg_2 >= arg_3:
+        if arg_1 >= arg_3:
+            return arg_2 + arg_1
+        else:
+            return arg_2 + arg_3
+    elif arg_3 >= arg_1 and arg_3 >= arg_2:
+        if arg_1 >= arg_2:
+            return arg_3 + arg_1
+        else:
+            return arg_3 + arg_2
+
+print(f'Сумма наибольших аргументов - {my_func(int(input("Введите первый аргумент: ")), int(input("введите второй аргумент: ")), int(input("Введите третий аргумент: ")))}')
